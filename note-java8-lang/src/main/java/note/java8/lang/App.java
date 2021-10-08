9bc08a28a2b0031754484688212123fbede3dd0a
Hosung <hs.gg.lee@gmail.com>
2021-10-08 15:00:30 +0900

    Adds note-java8-lang project.

---
+++ note-java8-lang/src/main/java/note/java8/lang/App.java
@@ -0,0 +1,13 @@
+package note.java8.lang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ note-java8-lang/src/test/java/note/java8/lang/AppTest.java
@@ -0,0 +1,20 @@
+package note.java8.lang;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
