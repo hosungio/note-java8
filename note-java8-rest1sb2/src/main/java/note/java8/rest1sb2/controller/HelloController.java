93563710ab6fe890dd969c07b68189b8eab9ab8e
Hosung <hs.gg.lee@gmail.com>
2021-10-11 17:34:39 +0900

    Adds a template for handling HTTP error.

--- note-java8-rest1sb2/src/main/java/note/java8/rest1sb2/controller/HelloController.java
+++ note-java8-rest1sb2/src/main/java/note/java8/rest1sb2/controller/HelloController.java
@@ -2,12 +2,13 @@ package note.java8.rest1sb2.controller;
 
 import java.time.ZonedDateTime;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import note.java8.rest1sb2.dto.HelloRequest;
 import note.java8.rest1sb2.dto.HelloResponse;
@@ -18,12 +19,12 @@ import note.java8.rest1sb2.dto.HelloWorld;
 public class HelloController {
 	
 	@GetMapping("")
-	public @ResponseBody HelloWorld getHelloWorld() {
+	public HelloWorld getHelloWorld() {
 		return new HelloWorld("Hello, World!", ZonedDateTime.now());
 	}
 
 	@PostMapping("")
-	public @ResponseBody HelloResponse getHello(
+	public HelloResponse getHello(
 			@RequestBody HelloRequest request) {
 		HelloResponse response = new HelloResponse(request.getId(),
 				request.getUser());
@@ -38,5 +39,23 @@ public class HelloController {
 			response.setMessage(request.getMessage());
 		}
 		return response;
+	}
+	
+	@GetMapping("err/404")
+	public void err404() {
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, 
+				"Your resource not found");
+	}
+	
+	@PostMapping("err/400")
+	public void err400(@RequestBody String body) {
+		System.out.println(body);
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, 
+				"Your request not valid");
+	}
+	
+	@GetMapping("err/500")
+	public void err500() {
+		throw new RuntimeException("A server error caused by ...");
 	}
 }
